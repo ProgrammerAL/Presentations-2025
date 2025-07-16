e1fd8e735bc08072c295e1f34704016768875ab5
Al Rodriguez <ProgrammerAL@outlook.com>
2025-07-16 19:00:15 -0400

    Added GLUGNET Meetup presentation

---
+++ glugnet-meetup-july-2025/csharp-pit-of-success/demo-code-feedback-system/FeedbackService/Configs.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.Options;
+
+namespace FeedbackService;
+
+public class ServiceConfig
+{
+    [Required(AllowEmptyStrings = false), NotNull]
+    public string? Environment { get; set; }
+
+    [Required(AllowEmptyStrings = false), NotNull]
+    public string? Version { get; set; }
+}
+
+[OptionsValidator]
+public partial class ServiceConfigValidateOptions : IValidateOptions<ServiceConfig>
+{
+}
+
+public class StorageConfig
+{
+    [Required(AllowEmptyStrings = false), NotNull]
+    public string? Endpoint { get; set; }
+
+    [Required(AllowEmptyStrings = false), NotNull]
+    public string? TableName { get; set; }
+}
+
+[OptionsValidator]
+public partial class StorageConfigValidateOptions : IValidateOptions<StorageConfig>
+{
+}
